2cd93dad4fec9f4ca1d2adc16ec3b1619d825077
andreafernandes <andreafernandes@users.noreply.github.com>
2017-09-27 14:54:20 +0100

    Update acf_misc_functions.R

--- acf_misc_functions.R
+++ acf_misc_functions.R
@@ -64,7 +64,7 @@ binarise.it.RA <- function(y) {
 
 ## date format --------------------------------------#
 date.format <- function(y) {
-  dateformat <- as.Date(y, format = "%d/%m/%Y", origin="1970-01-01")
+  dateformat <- (as.Date(y, format = "%d/%m/%Y", origin="1970-01-01"))
   return(dateformat)
 }
   
